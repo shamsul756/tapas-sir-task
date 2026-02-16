a3b026e307af50b68993b0964fe9a7cfe3129267
shamsul765 <webdevlopershamsul@gmail.com>
2026-02-17 00:25:54 +0600

    complete number five task

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-// Red Light... Green Light... Let's Play!
 
-// - [ ] Create a `color` variable.
-// - [ ] Based on the color variable's value print in the console if a traveller needs to STOP or GO. Red color is for STOP and Green color is for GO.
- 
-let color= 'ReD';
-if ( color.toLowerCase()  === 'red'){
-    console.log('stop here');
-}
-else if(color.toLowerCase() === 'green'){
-    console.log('it is time to go');
-}
-else{
-    console.log('wait for a valid light');
-}
+// ## 5. Create an Electricity Bill Calculator
+// Let's calculate how much you pay as electricity bil per month and annually?
 
+// - [ ] Create a `units` variable. Based on this value you will calculate the total electricity bill for a months.
+// - [ ] If each day you consume the `units` and each unit cost 150 rupees, how much will you be chnarged per month?
+// - If there is a 20% discount on the annual payment, how much will you be charged for an annual payment?
+
+let unitsPerDay = 150;
+let unitsPerUnit = 150;
+let monthBill = (unitsPerDay * unitsPerUnit)*30;
+let annualGross = monthBill * 12;
+let discount = (annualGross * 20) /100;
+let tap =  annualGross - discount;
+console.log(tap);
