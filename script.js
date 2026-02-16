4591ea150f77cf227aac1978cb3c0d0b795bda49
shamsul765 <webdevlopershamsul@gmail.com>
2026-02-17 00:17:02 +0600

    complete third task

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-// Let's check if you are elligible to get a driving license. The eligibility to get a driving licence is 18 years.
+// Red Light... Green Light... Let's Play!
 
-// - [ ] Manage `age` as a variable.
-// - [ ] Check if the age is elligible for a driving license and print it on the console accordingly.
-let age = 20;
-if(age >= 18){
-    console.log('you are eligible for license');
+// - [ ] Create a `color` variable.
+// - [ ] Based on the color variable's value print in the console if a traveller needs to STOP or GO. Red color is for STOP and Green color is for GO.
+ 
+let color= 'ReD';
+if ( color.toLowerCase()  === 'red'){
+    console.log('stop here');
+}
+else if(color.toLowerCase() === 'green'){
+    console.log('it is time to go');
 }
 else{
-    console.log('you are not eligible for license');
+    console.log('wait for a valid light');
 }
-// Let's calculate how much do you earn from your office.
 
-// - [ ] You get 12,300 rupees as your monthly salary.
-// - [ ] You get a 20% bonus on your annual salary.
-// - [ ] How much money do you make per annum as a CTC?
-
-let monthlySalary = 12300;
-let bonusPercentage = 20;
-
-let annualBase = monthlySalary * 12;
-let bonusAmount = annualBase * bonusPercentage / 100;
-let ctc = annualBase + bonusAmount;
-console.log('pocket amount :', ctc);
